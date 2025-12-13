18df553ba65be02e659873a4a162a53bc174b9f3
aditya-agniesh <aditya.agniesh@gmail.com>
2025-12-14 00:18:12 +0530

    testing update controller

--- Backend/src/test/java/com/example/sweetshop/Controller/SweetControllerTest.java
+++ Backend/src/test/java/com/example/sweetshop/Controller/SweetControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.sweetshop.Controller;
 
+import com.example.sweetshop.model.SweetCategory;
 import com.example.sweetshop.payload.SweetRequestDTO;
+import com.example.sweetshop.payload.SweetResponseDTO;
 import com.example.sweetshop.service.SweetService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,11 +17,22 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.sweetshop.exceptions.ResourceNotFoundException;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
 @WebMvcTest(SweetController.class)
 class SweetControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @MockitoBean
     private SweetService sweetService;
@@ -41,6 +55,58 @@ class SweetControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(request))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateSweet_shouldReturn200_whenSweetUpdatedSuccessfully() throws Exception {
+
+        Long sweetId = 1L;
+
+        SweetRequestDTO requestDTO = new SweetRequestDTO();
+        requestDTO.setSweetName("Kaju Katli");
+        requestDTO.setPrice(30);
+        requestDTO.setQuantity(10);
+        requestDTO.setSweetCategory(SweetCategory.DRY_FRUIT);
+
+        SweetResponseDTO responseDTO = new SweetResponseDTO();
+        responseDTO.setSweetName("Kaju Katli");
+        responseDTO.setPrice(30);
+        responseDTO.setQuantity(10);
+        responseDTO.setSweetCategory(SweetCategory.DRY_FRUIT);
+
+        when(sweetService.updateSweet(eq(sweetId), any(SweetRequestDTO.class)))
+                .thenReturn(responseDTO);
+
+        mockMvc.perform(
+                        put("/api/sweets/{sweetId}", sweetId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(requestDTO))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.sweetName").value("Kaju Katli"))
+                .andExpect(jsonPath("$.sweetCategory").value("DRY_FRUIT"));
+    }
+
+    @Test
+    void updateSweet_shouldReturn404_whenSweetNotFound() throws Exception {
+
+        Long sweetId = 99L;
+
+        SweetRequestDTO requestDTO = new SweetRequestDTO();
+        requestDTO.setSweetName("Rasgulla");
+        requestDTO.setPrice(20);
+        requestDTO.setQuantity(5);
+        requestDTO.setSweetCategory(SweetCategory.DRY_FRUIT);
+
+        when(sweetService.updateSweet(eq(sweetId), any(SweetRequestDTO.class)))
+                .thenThrow(new ResourceNotFoundException("Sweet not found"));
+
+        mockMvc.perform(
+                        put("/api/sweets/{sweetId}", sweetId)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(requestDTO))
+                )
+                .andExpect(status().isNotFound());
     }
 }
 
